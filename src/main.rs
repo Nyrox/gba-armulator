32b778d2c296b1d4cc8390114cf2442a9c690303
Nyrox <root@nyrox.tech>
2020-07-18 08:42:33 +0200

    Implements a few more ARM instructions and switch to THUMB mode

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 #![feature(const_generics)]
 #![feature(box_syntax)]
 #![feature(exclusive_range_pattern)]
-
 #![allow(incomplete_features)]
 
-use std::fs;
-use std::io::prelude::*;
 use bytemuck;
-use std::mem;
 use derivative::Derivative;
 use std::fmt;
+use std::fs;
+use std::io::prelude::*;
+use std::mem;
 
 #[derive(Copy, Clone)]
 struct SmallAsciiString<const N: usize> {
@@ -22,7 +21,6 @@ impl<const N: usize> fmt::Debug for SmallAsciiString<N> {
     }
 }
 
-
 type SmallAsciiString12 = SmallAsciiString<12>;
 type SmallAsciiString4 = SmallAsciiString<4>;
 type SmallAsciiString2 = SmallAsciiString<2>;
@@ -31,7 +29,10 @@ trait FormatBinary {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result;
 }
 
-impl<T> FormatBinary for T where T: bytemuck::Pod {
+impl<T> FormatBinary for T
+where
+    T: bytemuck::Pod,
+{
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         let bytes = bytemuck::bytes_of(self);
         let mut list = fmt.debug_list();
@@ -50,37 +51,38 @@ fn fmt_bin(v: &u32, fmt: &mut fmt::Formatter) -> fmt::Result {
 #[derive(Derivative, Clone, Copy)]
 #[derivative(Debug)]
 struct RomHeader {
-    #[derivative(Debug(format_with="fmt_bin"))]
+    #[derivative(Debug(format_with = "fmt_bin"))]
     entry_point: u32,
-    #[derivative(Debug="ignore")]
+    #[derivative(Debug = "ignore")]
     nintendo_logo: [u8; 156],
     game_title: SmallAsciiString12,
     game_code: SmallAsciiString4,
     maker_code: SmallAsciiString2,
-    fixed_value: u8, // 96h
+    fixed_value: u8,    // 96h
     main_unit_code: u8, // 00h for current GBA models
-    device_type: u8, // usually 00h
-    #[derivative(Debug="ignore")]
+    device_type: u8,    // usually 00h
+    #[derivative(Debug = "ignore")]
     _reserved1: [u8; 7], // should be zero filled
     software_version: u8, // usually 00h
     complement_check: u8, // header checksum
-    #[derivative(Debug="ignore")]
+    #[derivative(Debug = "ignore")]
     _reserved2: [u8; 2], // should be zero filled
 }
-
 
 fn sign_extend32(data: u32, size: u32) -> i32 {
     assert!(size > 0 && size <= 32);
     ((data << (32 - size)) as i32) >> (32 - size)
 }
 
-
 struct DebugIsHex<T> {
     inner: T,
 }
 
-impl<T> std::fmt::Debug for DebugIsHex<T> where T: std::fmt::LowerHex {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result { 
+impl<T> std::fmt::Debug for DebugIsHex<T>
+where
+    T: std::fmt::LowerHex,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         f.write_fmt(format_args!("0x{:x}", self.inner))
     }
 }
@@ -90,7 +92,6 @@ macro_rules! hex {
         DebugIsHex { inner: $id }
     };
 }
-
 
 struct Memory {
     pub system_rom: [u8; 16 * 1024],
@@ -121,22 +122,23 @@ impl Memory {
     }
 
     pub fn with_rom(rom: Vec<u8>) -> Self {
-        Memory { rom, ..Default::default() }
+        Memory {
+            rom,
+            ..Default::default()
+        }
     }
 
     pub fn index(&self, address: u32) -> Result<*const u8, MemoryError> {
         unsafe {
             match address {
-                0..0x00003FFF => {
-                    Ok(self.system_rom.as_ptr().offset(address as isize - 0))
-                },
+                0..0x00003FFF => Ok(self.system_rom.as_ptr().offset(address as isize - 0)),
                 0x03000000..0x03007FFF => {
                     Ok(self.wram_32.as_ptr().offset(address as isize - 0x03000000))
                 }
                 0x08000000..0x09FFFFFF => {
                     Ok(self.rom.as_ptr().offset(address as isize - 0x08000000))
                 }
-                _ => Err(MemoryError::OutOfRange(hex!(address)))
+                _ => Err(MemoryError::OutOfRange(hex!(address))),
             }
         }
     }
@@ -146,22 +148,32 @@ impl Memory {
     }
 }
 
+use num_traits::{Zero, One};
+
+fn get_bit<T>(num: T, bit: T) -> bool where T: num_traits::PrimInt {
+    let b = (num >> bit.to_usize().unwrap()) & One::one();
+    b == One::one()
+}
+
+fn get_bits<T>(num: T, offset: T, len: T) -> T where T: num_traits::PrimInt {
+    (num >> offset.to_usize().unwrap()) & !(!T::zero() << len.to_usize().unwrap())
+}
+
 fn main() {
     let mut rom: Vec<u8> = {
         let mut rom_buffer = Vec::new();
-        let mut rom_file = fs::File::open(r"./roms/Pokemon - Leaf Green Version (U) (V1.1).gba").unwrap();
+        let mut rom_file =
+            fs::File::open(r"./roms/Pokemon - Leaf Green Version (U) (V1.1).gba").unwrap();
         // let mut rom_file = fs::File::open(r"./roms/Pokemon - Sapphire Version (U) (V1.1).gba").unwrap();
         rom_file.read_to_end(&mut rom_buffer).unwrap();
         rom_buffer
     };
 
     let mut memory = box Memory::with_rom(rom);
-    
+
     assert_eq!(mem::size_of::<RomHeader>(), 192);
-    let header = unsafe {
-        *(memory.rom.as_ptr() as *const RomHeader)
-    };
-    
+    let header = unsafe { *(memory.rom.as_ptr() as *const RomHeader) };
+
     println!("{:#?}", header);
 
     let mut registers: [u32; 16] = [0; 16];
@@ -175,13 +187,43 @@ fn main() {
 
     *pc = 0x08000000;
     loop {
-        print!("\n");
-        let instruction = unsafe {
-            *(memory.index(*pc).unwrap() as *const u32)
+        let is_thumb_mode = |f: u8| {
+            (f >> 5) & 1 == 1
         };
+        if is_thumb_mode(cpsr_flags[0]) {
+            let instruction = unsafe {
+                *(memory.index(*pc).unwrap() as *const u16)
+            };
+
+            dbg!(format!("0x{:x}", *pc));
+            *pc = *pc + 4;
+
+            // do shit
+            match get_bits(instruction, 13, 3) {
+                0b000 => {
+                    let opcode = get_bits(instruction, 11, 2);
+                    match opcode {
+                        0b11 => {
+
+                        },
+                        _ => unimplemented!()
+                    }
+                }
+                _ => unimplemented!()
+            }
+
+            *pc = *pc - 2;
+            continue;
+        } 
+
+        //
+        //  END OF THUMB MODE
+        // 
+
+        print!("\n");
+        let instruction = unsafe { *(memory.index(*pc).unwrap() as *const u32) };
 
         *pc = *pc + 8;
-        
 
         let _registerPrinted = registers.iter().map(|e| hex!(e)).collect::<Vec<_>>();
         dbg!(_registerPrinted);
@@ -189,7 +231,7 @@ fn main() {
         dbg!(format!("0x{:x}", (*pc) - 8));
 
         let instr_bytes = bytemuck::bytes_of(&instruction);
-        
+
         let cond_block = instr_bytes[3] & 0b11110000;
 
         dbg!(format!("{:#b}", instruction));
@@ -205,17 +247,10 @@ fn main() {
             }
         };
 
-        if !cond { panic!() }
-
-
-        fn get_bit(num: u32, bit: u32) -> bool {
-            let b = (num >> bit) & 1;
-            b == 1
+        if !cond {
+            panic!()
         }
 
-        fn get_bits(num: u32, offset: u32, len: u32) -> u32 {
-            (num >> offset) & !(!0 << len)
-        }        
 
         match op_block {
             // branch
@@ -227,7 +262,76 @@ fn main() {
                 let offset = instruction & 0x00FFFFFF;
                 *pc = (*pc as i32 + sign_extend32(offset << 2, 26)) as u32;
                 continue;
-            },
+            }
+            // misc instructions
+            _ if (instruction & 0x0FB000F0) == 0x01000000 => {
+                dbg!("misc");
+
+                unimplemented!()
+            }
+            // BX
+            _ if (instruction & 0x0FF000F0) == 0x01200010 => {
+                let rm = get_bits(instruction, 0, 4);
+                let t_flag = get_bits(rm, 0, 1);
+                dbg!(t_flag);
+                cpsr_flags[0] = (cpsr_flags[0] & 0b11011111) | (t_flag << 5) as u8;             
+                *pc = registers[rm as usize] & !1;
+ 
+                continue;
+            }
+            // register to status register
+            _ if (instruction & 0x0FB000F0) == 0x01200000 => {
+                let r_bit = get_bit(instruction, 22);
+                dbg!(r_bit);
+
+                let field_mask = (instruction & 0xF0000) >> 16;
+                dbg!(field_mask);
+
+                let operand = registers[(instruction & 0xF) as usize];
+
+                if r_bit {
+                    unimplemented!()
+                }
+
+                for i in 0..=3 {
+                    if get_bit(field_mask, i as u32) {
+                        cpsr_flags[i] = ((operand >> (i * 8)) & 0xFF) as u8;
+                    }
+                }
+            }
+            // some kind of data-processing shift
+            _ if (op_block & 0xE0) == 0x00 => {
+                let opcode = get_bits(instruction, 21, 4);
+                let s_flag = op_block & 0x001;
+                let rd = get_bits(instruction, 12, 4);
+                let rn = get_bits(instruction, 16, 4);
+
+                let register_shift = get_bit(instruction, 4);
+
+                match opcode {
+                    // mov
+                    0b1101 => {
+                        let is_register_shift = get_bit(instruction, 4);
+                        if is_register_shift {
+                            unimplemented!()
+                        }
+                        let shift_mode = get_bits(instruction, 5, 3);
+                        if shift_mode != 0 {
+                            unimplemented!()
+                        }
+
+                        let rm = get_bits(instruction, 0, 4);
+                        let shift_imm = get_bits(instruction, 7, 5);
+
+                        if s_flag != 0 {
+                            unimplemented!()
+                        }
+
+                        registers[rd as usize] = registers[rm as usize] << shift_imm;
+                    }
+                    _ => unimplemented!(),
+                }
+            }
             // move immediate to status register
             _ if (op_block & 0xFB) == 0x32 => {
                 dbg!("move immediate to status register");
@@ -250,13 +354,17 @@ fn main() {
 
                         let rd = get_bits(instruction, 12, 4);
 
-                        if s_flag != 0 { unimplemented!() }
+                        if s_flag != 0 {
+                            unimplemented!()
+                        }
 
                         registers[rd as usize] = immed_8 >> rotate;
-                    },
+                    }
                     // add
                     0b0100 => {
-                        if s_flag != 0 { unimplemented!() }
+                        if s_flag != 0 {
+                            unimplemented!()
+                        }
 
                         let immed_8 = get_bits(instruction, 0, 8);
                         let rotate = get_bits(instruction, 8, 4);
@@ -268,42 +376,25 @@ fn main() {
 
                         registers[rd as usize] = registers[rn as usize] + (immed_8 >> rotate);
                     }
-                    _ => unimplemented!()
-                }
-            },
-            // move register to status register with register operand
-            _ if (instruction & 0x0FB00010) == 0x01200000 => {
-                dbg!("MSR - Reg");
-
-                let r_bit = get_bit(instruction, 22);
-                dbg!(r_bit);
-
-                let field_mask = (instruction & 0xF0000) >> 16;
-                dbg!(field_mask);
-
-                let operand = registers[(instruction & 0xF) as usize];
-
-                if r_bit { unimplemented!() }
-
-                for i in 0..=3 {
-                    if get_bit(field_mask, i as u32) {
-                        cpsr_flags[i] = ((operand >> (i * 8)) & 0xFF) as u8;
-                    }
+                    _ => unimplemented!(),
                 }
             }
             // some kind of load/store
             _ if (op_block & 0xC0) == 0x40 => {
-                
                 // immediate
                 let immediate = !get_bit(instruction, 25);
                 let load = get_bit(instruction, 20);
                 let is_byte = get_bit(instruction, 22);
-                
+
                 let rd = get_bits(instruction, 12, 4);
                 let rn = get_bits(instruction, 16, 4);
 
-                if is_byte { unimplemented!() }
-                if !immediate { unimplemented!() }                
+                if is_byte {
+                    unimplemented!()
+                }
+                if !immediate {
+                    unimplemented!()
+                }
 
                 let offset = get_bits(instruction, 0, 12);
                 let base = registers[rn as usize];
@@ -317,13 +408,12 @@ fn main() {
                 unsafe {
                     if load {
                         registers[rd as usize] = *mem_loc;
-                    }
-                    else{
+                    } else {
                         *mem_loc = registers[rd as usize];
                     }
                 }
-            } 
-            _ => unimplemented!()
+            }
+            _ => unimplemented!(),
         }
 
         *pc = *pc - 4;
